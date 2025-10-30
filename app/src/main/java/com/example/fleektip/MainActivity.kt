01c2b296b726706ad1864709305489bd1286ded4
Jeff Tan <jefftan974@gmail.com>
2025-10-30 19:39:58 +0800

    Added Buttons for opening Socials (Facebook and Tiktok).
    UI for AR screen still on development

--- app/src/main/java/com/example/fleektip/MainActivity.kt
+++ app/src/main/java/com/example/fleektip/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.widget.Button
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.ImageButton
+import android.net.Uri
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,16 +13,38 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_menu)
 
+        // Move to AR try on screen
         val btnTryOn = findViewById<Button>(R.id.btnTryOn)
         btnTryOn.setOnClickListener {
             val intent = Intent(this, ARScreenActivity::class.java)
             startActivity(intent)
         }
 
+        //Move to reservation Screen
         val btnReservation = findViewById<Button>(R.id.btnReservation)
         btnReservation.setOnClickListener {
             val intent = Intent(this, ReservationActivity::class.java)
             startActivity(intent)
+        }
+
+        //Buttons for opening the link for the socials
+        val btnFacebook = findViewById<ImageButton>(R.id.btn_facebook)
+        val btnTiktok = findViewById<ImageButton>(R.id.btn_tiktok)
+
+        //Shows loading screen first before opening app oe browser
+
+        // Facebook button
+        btnFacebook.setOnClickListener {
+            val loadingIntent = Intent(this, SocialRedirectActivity::class.java)
+            loadingIntent.putExtra("platform", "facebook")
+            startActivity(loadingIntent)
+        }
+
+        // TikTok button
+        btnTiktok.setOnClickListener {
+            val loadingIntent = Intent(this, SocialRedirectActivity::class.java)
+            loadingIntent.putExtra("platform", "tiktok")
+            startActivity(loadingIntent)
         }
 
     }
--- app/src/main/java/com/example/fleektip/ReservationActivity.kt
+++ app/src/main/java/com/example/fleektip/ReservationActivity.kt
@@ -4,13 +4,14 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.ImageButton
 
 class ReservationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.reservation)
 
-        val btnBack = findViewById<Button>(R.id.btnBack)
+        val btnBack = findViewById<ImageButton>(R.id.btnBack)
         btnBack.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
